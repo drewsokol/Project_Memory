92511a91c7ccc86649c855104c74f2a461224762
drewsokol <106027926+drewsokol@users.noreply.github.com>
2024-06-02 13:44:03 -0400

    This commit should fail to push

--- assets/test.gd
+++ assets/test.gd
@@ -2,6 +2,7 @@ extends Node
 
 
 # Called when the node enters the scene tree for the first time.
+# this change should not be committed to master without a PR
 func _ready():
 	pass # Replace with function body.
 
